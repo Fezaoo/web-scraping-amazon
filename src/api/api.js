7ae27aca5d2af1e5c2321c89d46b4998900d7b39
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-05-10 15:59:34 -0300

    cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
