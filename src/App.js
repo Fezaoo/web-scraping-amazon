0d1769e2678c331f6d72a485de22413007ebf403
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-05-13 21:04:50 -0300

    Revert "api"
    This reverts commit 92823ca77ac6cad5c4127be744a9fa4d5488d31d.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       setLoading(true)
       if (!Display) { setDisplay(true) }
       setActualSearch(Search)
-      axios.get(`https://api-amazon-web-scraping-9qxnatgvl-fezaoos-projects.vercel.app/api/dados?query=${Search}&limit=${Limit}`) // Requisição da API.  
+      axios.get(`http://127.0.0.1:5000/api/dados?query=${Search}&limit=${Limit}`) // Requisição da API.  
         .then(response => {
           setData(response.data)
           console.log('Dados recebidos:', response.data);
